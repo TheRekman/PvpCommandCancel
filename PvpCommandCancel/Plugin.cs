a4894c8c6a2358706fa8bf358a39ada3a365de91
Rekman <rekman.heln@gmail.com>
2019-11-30 16:48:34 +0300

    Add config command

--- PvpCommandCancel/Plugin.cs
+++ PvpCommandCancel/Plugin.cs
@@ -15,13 +15,50 @@ namespace PvpCommandCancel
         public override string Author => "Rekman";
         public override string Description => "Cancel command use in PvP.";
 
+        Config Config;
         public Plugin(Main game) : base(game)
         {
 
         }
         public override void Initialize()
         {
-            
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+        }
+
+        private void OnInitialize(EventArgs args)
+        {
+            Config = Config.Read();
+            Commands.ChatCommands.Add(new Command("pvpcmdcancel.configcmd", ConfigCmd, "/pcc", "/pvpcmdcancel"));
+        }
+
+        private void ConfigCmd(CommandArgs args)
+        {
+            if (args.Parameters.Count < 1) args.Parameters.Add("help");
+
+            switch (args.Parameters[0])
+            {
+                case "add":
+                    if(args.Parameters.Count == 2)
+                    {
+                        Command cmd = Commands.ChatCommands.Find(c => c.Names.First(n => n == args.Parameters[1]) != null);
+                        if(cmd != null)
+                        {
+                            Config.WriteUnallowedCommand(cmd.Name);
+                            args.Player.SendSuccessMessage($"Succesfully unallow {cmd.Name} command.");
+                        }
+                        else
+                        {
+                            args.Player.SendErrorMessage($"Invalid command {args.Parameters[1]}!");
+                        }
+                    }
+                    else
+                    {
+                        args.Player.SendErrorMessage("Invalid syntax! Proper syntax: {0}/pcc add <commandName>");
+                    }
+                    break;
+                case "reset":
+                    break;
+            }
         }
 
         protected override void Dispose(bool disposing)

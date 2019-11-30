6155e5fb988ff5bb7b0d094e724d307cbb43d23b
Rekman <rekman.heln@gmail.com>
2019-11-30 16:11:08 +0300

    plugin base

---
+++ PvpCommandCancel/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+namespace PvpCommandCancel
+{
+    public class Plugin : TerrariaPlugin
+    {
+        public override string Name => "PvpCommandCancel";
+        public override Version Version => System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+        public override string Author => "Rekman";
+        public override string Description => "Cancel command use in PvP.";
+
+        public Plugin(Main game) : base(game)
+        {
+
+        }
+        public override void Initialize()
+        {
+            
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
